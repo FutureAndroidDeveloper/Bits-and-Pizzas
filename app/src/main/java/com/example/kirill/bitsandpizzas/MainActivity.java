e24201e33c9248c1cd9d3b799d453dc561efcb6b
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-01 23:57:12 +0300

    Добавил обработку нажатия на действие

--- app/src/main/java/com/example/kirill/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/kirill/bitsandpizzas/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.kirill.bitsandpizzas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
