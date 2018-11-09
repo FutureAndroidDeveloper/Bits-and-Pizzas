a721be6747f8846170d1165ccdd160f40255df4b
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-09 23:26:37 +0300

    Realized onClickDone with snackbar

--- app/src/main/java/com/example/kirill/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/kirill/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,12 @@
 package com.example.kirill.bitsandpizzas;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,5 +20,19 @@ public class OrderActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void onClickDone(View view) {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), "Your order has been updated", Snackbar.LENGTH_LONG);
+
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+            }
+        });
+
+        snackbar.show();
     }
 }
