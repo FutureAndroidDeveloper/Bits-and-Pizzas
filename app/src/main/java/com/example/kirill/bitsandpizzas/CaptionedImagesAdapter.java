3304fa86b1a08038594930cca0c2e26daf3cf9d7
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-11 15:59:24 +0300

    Create onBindViewHolder method

--- app/src/main/java/com/example/kirill/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/kirill/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,9 +1,14 @@
 package com.example.kirill.bitsandpizzas;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
     private String[] names;
@@ -41,4 +46,15 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
     }
 
 
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        ImageView imageView = (ImageView) cardView.findViewById(R.id.info_image);
+        Drawable drawable =
+                ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(names[position]);
+        TextView textView = (TextView)cardView.findViewById(R.id.info_text);
+        textView.setText(names[position]);
+    }
 }
