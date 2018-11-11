8a7d38a6474330d088f45b4ed3e3a8cd6a1e424a
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-11 15:51:04 +0300

    Create onCreateViewHolder method

--- app/src/main/java/com/example/kirill/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/kirill/bitsandpizzas/CaptionedImagesAdapter.java
@@ -2,6 +2,8 @@ package com.example.kirill.bitsandpizzas;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
     private String[] names;
@@ -28,4 +30,15 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
     public int getItemCount() {
         return names.length;
     }
+
+
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_captioned_image, parent, false);
+        return new ViewHolder(cv);
+    }
+
+
 }
